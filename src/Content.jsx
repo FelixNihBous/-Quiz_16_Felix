a9262f41d29a30dcb602ea638a0538f2a905bf90
FelixNihBous <felixlix604@gmail.com>
2024-11-14 22:05:23 +0700

    Added Main Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import profilePic from './Component/assets/profile-pic.png'
+import photoImg from './Component/assets/gallery.png'
 import Header from './Component/Header.jsx';
 import './App.css'
 import Sidebar from './Component/Sidebar.jsx';
@@ -17,6 +18,7 @@ function Content() {
             <img src={profilePic} className='profilesize'></img>
             <div className='text-area'>
               <textarea placeholder='What happened today?'></textarea>
+              <img className='imgsize' src={photoImg} alt="" />
             </div>
           </div>
         </div>
